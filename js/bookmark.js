468162aeaf4a85d1fd02d25f83683d262faeca5d
lpdink <lpdink@users.noreply.github.com>
2024-07-13 10:22:30 +0000

    deploy: deac383f8084c6274a407f3c8815e3faf27542e9

--- js/bookmark.js
+++ js/bookmark.js
@@ -1,56 +1,56 @@
 /* global CONFIG */
 
 document.addEventListener('DOMContentLoaded', () => {
-  'use strict';
+    'use strict';
 
-  const doSaveScroll = () => {
-    localStorage.setItem('bookmark' + location.pathname, window.scrollY);
-  };
+    const doSaveScroll = () => {
+        localStorage.setItem('bookmark' + location.pathname, window.scrollY);
+    };
 
-  const scrollToMark = () => {
-    let top = localStorage.getItem('bookmark' + location.pathname);
-    top = parseInt(top, 10);
-    // If the page opens with a specific hash, just jump out
-    if (!isNaN(top) && location.hash === '') {
-      // Auto scroll to the position
-      window.anime({
-        targets  : document.scrollingElement,
-        duration : 200,
-        easing   : 'linear',
-        scrollTop: top
-      });
-    }
-  };
-  // Register everything
-  const init = function(trigger) {
-    // Create a link element
-    const link = document.querySelector('.book-mark-link');
-    // Scroll event
-    window.addEventListener('scroll', () => link.classList.toggle('book-mark-link-fixed', window.scrollY === 0), { passive: true });
-    // Register beforeunload event when the trigger is auto
-    if (trigger === 'auto') {
-      // Register beforeunload event
-      window.addEventListener('beforeunload', doSaveScroll);
-      document.addEventListener('pjax:send', doSaveScroll);
-    }
-    // Save the position by clicking the icon
-    link.addEventListener('click', () => {
-      doSaveScroll();
-      window.anime({
-        targets : link,
-        duration: 200,
-        easing  : 'linear',
-        top     : -30,
-        complete: () => {
-          setTimeout(() => {
-            link.style.top = '';
-          }, 400);
+    const scrollToMark = () => {
+        let top = localStorage.getItem('bookmark' + location.pathname);
+        top = parseInt(top, 10);
+        // If the page opens with a specific hash, just jump out
+        if (!isNaN(top) && location.hash === '') {
+            // Auto scroll to the position
+            window.anime({
+                targets: document.scrollingElement,
+                duration: 20,
+                easing: 'linear',
+                scrollTop: top
+            });
         }
-      });
-    });
-    scrollToMark();
-    document.addEventListener('pjax:success', scrollToMark);
-  };
+    };
+    // Register everything
+    const init = function (trigger) {
+        // Create a link element
+        const link = document.querySelector('.book-mark-link');
+        // Scroll event
+        window.addEventListener('scroll', () => link.classList.toggle('book-mark-link-fixed', window.scrollY === 0), { passive: true });
+        // Register beforeunload event when the trigger is auto
+        if (trigger === 'auto') {
+            // Register beforeunload event
+            window.addEventListener('beforeunload', doSaveScroll);
+            document.addEventListener('pjax:send', doSaveScroll);
+        }
+        // Save the position by clicking the icon
+        link.addEventListener('click', () => {
+            doSaveScroll();
+            window.anime({
+                targets: link,
+                duration: 20,
+                easing: 'linear',
+                top: -30,
+                complete: () => {
+                    setTimeout(() => {
+                        link.style.top = '';
+                    }, 400);
+                }
+            });
+        });
+        scrollToMark();
+        document.addEventListener('pjax:success', scrollToMark);
+    };
 
-  init(CONFIG.bookmark.save);
+    init(CONFIG.bookmark.save);
 });
